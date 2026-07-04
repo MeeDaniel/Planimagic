6d19a62aec18bb3d829181d889436afa7f0f9b5f
MeeDaniel <danya.cnh@gmail.com>
2026-07-04 12:39:55 +0300

    feat(graphics): points are draggable now

--- graphics/app.py
+++ graphics/app.py
@@ -2,7 +2,7 @@ from typing import Union, Tuple, List, Callable
 import pygame_app as pa
 import pygame
 
-from core import System
+from core import System, Point
 from .graphics_config import GraphicsConfig
 
 
@@ -17,31 +17,70 @@ class App(pa.App):
             title: Union[str, None] = None,
             window_flags: int = 0
     ):
+        # === Settings ===
         super().__init__(window, tps, title, window_flags | pygame.SRCALPHA)
         self.system = system
         self.config = config
         self.reload_system_method = reload_system_method
+        
+        # === Graphics ===
+        self.__transparent_surface = pygame.Surface(window, pygame.SRCALPHA)
+
+        # === Logic ===
+        self.__grabbed_point: Union[Point, None] = None
     
     def update(self) -> None:
         self.reload_system_method()
-    
+        self.move_points()
+
     def draw(self) -> None:
+        self.screen.fill(self.config.BACKGROUND_COLOR)
+        self.__transparent_surface.fill((0, 0, 0, 0))
+
         points = self.system.get_points()
         for name, point in points.items():
             x, y = point.get_pos()
             outer_color = pygame.Color(point.color)
-            outer_color.a = 128
+            outer_color.a = 64
 
             pygame.draw.circle(
-                self.screen,
+                self.__transparent_surface,
                 outer_color,
                 (x, y),
                 self.config.POINT_OUTER_RADIUS
             )
 
             pygame.draw.circle(
-                self.screen,
+                self.__transparent_surface,
                 point.color,
                 (x, y),
                 self.config.POINT_INNER_RADIUS
             )
+        
+        self.screen.blit(self.__transparent_surface, (0, 0))
+
+    def move_points(self) -> None:
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_rel = pygame.mouse.get_rel()
+
+        cursor = pygame.SYSTEM_CURSOR_ARROW
+
+        if self.__grabbed_point is None:
+            for name, point in self.system.get_points().items():
+                if (mouse_pos[0] - point.get_pos()[0]) ** 2 + (mouse_pos[1] - point.get_pos()[1]) ** 2 \
+                        <= self.config.POINT_OUTER_RADIUS ** 2:
+                    cursor = pygame.SYSTEM_CURSOR_HAND
+                    if self.mouse.is_pressed[0]:
+                        self.__grabbed_point = point
+                    break
+        else:
+            if self.mouse.is_pressed[0]:
+                cursor = pygame.SYSTEM_CURSOR_HAND
+                self.__grabbed_point.set_pos(
+                    self.__grabbed_point.get_pos()[0] + mouse_rel[0],
+                    self.__grabbed_point.get_pos()[1] + mouse_rel[1]
+                )
+            else:
+                self.__grabbed_point = None
+        
+        pygame.mouse.set_cursor(cursor)
--- graphics/graphics_config.py
+++ graphics/graphics_config.py
@@ -3,5 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class GraphicsConfig:
+    # === Sizes ===
     POINT_INNER_RADIUS = 5
-    POINT_OUTER_RADIUS = 15
+    POINT_OUTER_RADIUS = 13
+
+    # === Colors ===
+    BACKGROUND_COLOR = "#171717"
