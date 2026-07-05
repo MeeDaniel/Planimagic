54236f965a572bda386533f73513a75ef235bf14
MeeDaniel <danya.cnh@gmail.com>
2026-07-05 13:50:04 +0300

    feat(core/segment): add method get_pos_by_proportion

--- core/segment.py
+++ core/segment.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Tuple
 from util.definitions import ColorValue
 
 from .shape import Shape
@@ -14,3 +14,12 @@ class Segment(Shape):
             color: Union[ColorValue, None] = None
         ):
         super().__init__([from_, to], name, color)
+    
+    def get_pos_by_proportion(self, proportion: float) -> Tuple[float, float]:
+        from_, to = self.get_key_points()
+        from_x, from_y = from_.get_pos()
+        to_x, to_y = to.get_pos()
+        return (
+            from_x + (to_x - from_x) * proportion,
+            from_y + (to_y - from_y) * proportion
+        )
