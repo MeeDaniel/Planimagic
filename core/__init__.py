57c63e7954f82cc9c8bd07787f80c8af1ca43ea2
MeeDaniel <danya.cnh@gmail.com>
2026-07-05 12:18:45 +0300

    feat(core): add new shape "segment"

--- core/__init__.py
+++ core/__init__.py
@@ -3,3 +3,4 @@ from .general_field import GeneralField  # noqa: F401
 from .point import Point  # noqa: F401
 from .shape import Shape  # noqa: F401
 from .system import System  # noqa: F401
+from .segment import Segment  # noqa: F401
